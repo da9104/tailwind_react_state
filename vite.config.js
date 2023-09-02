474413f430a7b3dcc663b5e684c5dde4989bb81f
DAMI <damikang@DAMIs-MacBook-Pro.local>
2023-09-02 12:37:47 +0100

    adjusted config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base : "/tailwind_react_state",
+  plugins: [react()]
 })
